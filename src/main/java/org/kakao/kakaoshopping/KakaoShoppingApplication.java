160658343669e0e39cd5dd28ab1cbe0c6807d179
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-20 14:22:43 +0900

    test: 커밋 테스트

--- src/main/java/org/kakao/kakaoshopping/KakaoShoppingApplication.java
+++ src/main/java/org/kakao/kakaoshopping/KakaoShoppingApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class KakaoShoppingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KakaoShoppingApplication.class, args);
 	}
-
 }
