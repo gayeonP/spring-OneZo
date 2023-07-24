3cb713215ce98404f458c142bec87a7c38a02935
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-24 15:51:29 +0900

    refactor: LoginUserArgumentResolver 클래스명 변경

--- src/main/java/org/kakao/kakaoshopping/web/argumentResolver/LoginUserArgumentResolver.java
+++ src/main/java/org/kakao/kakaoshopping/web/argumentResolver/LoginUserArgumentResolver.java
@@ -9,7 +9,8 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		// 컨트롤러의 파라미터를 체크하는데 내부적으로 캐시가 동작하여 한 번 체크하면 다음부터는 캐시에 저장된 값을 사용함
@@ -26,7 +27,7 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
 		long memberId = Long.parseLong(webRequest.getParameter("id"));
 
 		return LoggedInUser.builder()
-			.memberId(memberId)
+			.userId(memberId)
 			.build();
 	}
 }
