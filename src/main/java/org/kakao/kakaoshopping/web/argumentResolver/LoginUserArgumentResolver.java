35486419af87c57a2d10385f0a19ea4797d05bc9
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-24 15:10:42 +0900

    refactor: LoginUser 클래스의 필드명을 userId로 수정

--- src/main/java/org/kakao/kakaoshopping/web/argumentResolver/LoginUserArgumentResolver.java
+++ src/main/java/org/kakao/kakaoshopping/web/argumentResolver/LoginUserArgumentResolver.java
@@ -1,7 +1,7 @@
 package org.kakao.kakaoshopping.web.argumentResolver;
 
-import org.kakao.kakaoshopping.web.annotaion.LoginMember;
-import org.kakao.kakaoshopping.web.dto.member.login.LoggedInMember;
+import org.kakao.kakaoshopping.web.annotaion.LoginUser;
+import org.kakao.kakaoshopping.web.dto.member.login.LoggedInUser;
 import org.springframework.core.MethodParameter;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -13,8 +13,8 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
 	@Override
 	public boolean supportsParameter(MethodParameter parameter) {
 		// 컨트롤러의 파라미터를 체크하는데 내부적으로 캐시가 동작하여 한 번 체크하면 다음부터는 캐시에 저장된 값을 사용함
-		boolean hasLoginAnnotation = parameter.hasParameterAnnotation(LoginMember.class);
-		boolean hasLoginUserType = LoggedInMember.class.isAssignableFrom(parameter.getParameterType());
+		boolean hasLoginAnnotation = parameter.hasParameterAnnotation(LoginUser.class);
+		boolean hasLoginUserType = LoggedInUser.class.isAssignableFrom(parameter.getParameterType());
 
 		return hasLoginAnnotation && hasLoginUserType;
 	}
@@ -25,7 +25,7 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
 
 		long memberId = Long.parseLong(webRequest.getParameter("id"));
 
-		return LoggedInMember.builder()
+		return LoggedInUser.builder()
 			.memberId(memberId)
 			.build();
 	}
