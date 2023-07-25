76510445086f8b6accc0b839a73dd5dfd3b5a19b
gayeonP <pagaye@naver.com>
2023-07-25 10:12:21 +0900

    refactor: 코드 설명 주석 추가

--- src/main/java/org/kakao/kakaoshopping/web/dto/item/request/ReadItem.java
+++ src/main/java/org/kakao/kakaoshopping/web/dto/item/request/ReadItem.java
@@ -4,7 +4,13 @@ import org.kakao.kakaoshopping.domain.entity.item.Item;
 
 import lombok.Builder;
 import lombok.Data;
+import org.kakao.kakaoshopping.domain.entity.item.Item;
 
+/**
+ * 기능 : 특정 상품을 담기 위한 DTO
+ * 작성자 - 박가연
+ * 작성일 - 2023.07.24
+ */
 @Data
 public class ReadItem {
 
