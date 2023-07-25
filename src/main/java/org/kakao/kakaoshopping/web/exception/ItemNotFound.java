9f2f96e53eb2b202ebbc6128649e23078be4a4d4
gayeonP <pagaye@naver.com>
2023-07-25 10:12:21 +0900

    refactor: 코드 설명 주석 추가

--- src/main/java/org/kakao/kakaoshopping/web/exception/ItemNotFound.java
+++ src/main/java/org/kakao/kakaoshopping/web/exception/ItemNotFound.java
@@ -2,18 +2,23 @@ package org.kakao.kakaoshopping.web.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class ItemNotFound extends KakaoException{
+/**
+ * 기능: 상품에 대한 정보가 DB 에 없을 때 예외 처리
+ * 작성자: 장규민
+ * 작성일: 2023.07.24
+ */
+public class ItemNotFound extends KakaoException {
 
-	public ItemNotFound(String message){
-		super(message);
-	}
+    public ItemNotFound(String message) {
+        super(message);
+    }
 
-	public ItemNotFound(String message, Throwable cause){
-		super(message, cause);
-	}
+    public ItemNotFound(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-	@Override
-	public HttpStatus getHttpStatus() {
-		return HttpStatus.BAD_REQUEST;
-	}
+    @Override
+    public HttpStatus getHttpStatus() {
+        return HttpStatus.BAD_REQUEST;
+    }
 }
