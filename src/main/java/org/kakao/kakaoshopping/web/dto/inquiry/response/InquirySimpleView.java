7323c772c88106a10e44919ed024a7474c896bf2
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-26 14:49:27 +0900

    refactor: username로 필드 수정, commentSize 필드 추가

--- src/main/java/org/kakao/kakaoshopping/web/dto/inquiry/response/InquirySimpleView.java
+++ src/main/java/org/kakao/kakaoshopping/web/dto/inquiry/response/InquirySimpleView.java
@@ -2,26 +2,26 @@ package org.kakao.kakaoshopping.web.dto.inquiry.response;
 
 import org.kakao.kakaoshopping.domain.entity.inquiry.Inquiry;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-// @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@NoArgsConstructor
 public class InquirySimpleView {
 
 	// 게시물 Id
 	private Long id;
-	// 작성자 Id
-	private Long memberId;
+	// 작성자 이름
+	private String username;
 	// 글 제목
 	private String title;
+	// 글 제목
+	private int commentSize;
 
 	public InquirySimpleView(Inquiry inquiry) {
 		this.id = inquiry.getId();
-		this.memberId = inquiry.getUser().getId();
+		this.username = inquiry.getUser().getName();
 		this.title = inquiry.getTitle();
+		this.commentSize = inquiry.getComments().size();
 	}
 }
