c5dd4a93394a074e4ce2d8587eaac3679b20cb0c
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-07-27 09:18:44 +0900

    feat: 장바구니 조회 화면 추가

--- src/main/java/org/kakao/kakaoshopping/web/controller/cart/CartController.java
+++ src/main/java/org/kakao/kakaoshopping/web/controller/cart/CartController.java
@@ -1,60 +1,66 @@
 package org.kakao.kakaoshopping.web.controller.cart;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.kakao.kakaoshopping.domain.entity.cart.Cart;
 import org.kakao.kakaoshopping.domain.service.cart.CartService;
 import org.kakao.kakaoshopping.web.annotaion.LoginUser;
 import org.kakao.kakaoshopping.web.dto.cart.request.CreateCart;
 import org.kakao.kakaoshopping.web.dto.cart.request.EditCart;
-import org.kakao.kakaoshopping.web.dto.member.login.LoggedInUser;
+import org.kakao.kakaoshopping.web.dto.user.login.LoggedInUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class CartController {
 
-    private final CartService cartService;
+	private final CartService cartService;
 
-    // 장바구니 담기
-    @PostMapping("/createCart")
-    public String createCart(CreateCart createCart, Long itemId, @LoginUser LoggedInUser loggedInUser) {
-        cartService.addCart(createCart.toEntity(), itemId, loggedInUser.getUserId());
-        return "redirect:/user/carts";
-    }
+	// 장바구니 담기
+	@PostMapping("/createCart")
+	public String createCart(CreateCart createCart, Long itemId, @LoginUser LoggedInUser loggedInUser) {
+		cartService.addCart(createCart.toEntity(), itemId, loggedInUser.getUserId());
+		return "redirect:/user/carts";
+	}
 
-    // 장바구니 목록 보여주기
-    @GetMapping("/carts")
-    public String viewCarts(Model model, @LoginUser LoggedInUser loggedInUser) {
-        List<Cart> carts = cartService.getItemsInCart(loggedInUser.getUserId());
-        model.addAttribute("items", carts);
-        return "/cartViews";
-    }
+	// 장바구니 목록 보여주기
+	@GetMapping("/carts")
+	public String viewCarts(Model model, @LoginUser LoggedInUser loggedInUser) {
+		List<Cart> carts = cartService.getItemsInCart(loggedInUser.getUserId());
+		model.addAttribute("items", carts);
+		return "/cartViews";
+	}
 
-    // 장바구니 안 아이템 삭제
-    @PostMapping("/deleteItemInCart")
-    public String deleteItemInCart(EditCart cart) {
-        cartService.deleteItemInCart(cart.toEntity());
-        return "redirect:/user/carts";
-    }
+	// 장바구니 안 아이템 삭제
+	@PostMapping("/deleteItemInCart")
+	public String deleteItemInCart(EditCart cart) {
+		cartService.deleteItemInCart(cart.toEntity());
+		return "redirect:/user/carts";
+	}
 
-    // 장바구니 자체 삭제
-    @PostMapping("/deleteCart")
-    public String deleteCart(@LoginUser LoggedInUser loggedInUser) {
-        cartService.deleteCart(loggedInUser.getUserId());
-        return "redirect:/user/carts";
-    }
+	// 장바구니 자체 삭제
+	@PostMapping("/deleteCart")
+	public String deleteCart(@LoginUser LoggedInUser loggedInUser) {
+		cartService.deleteCart(loggedInUser.getUserId());
+		return "redirect:/user/carts";
+	}
 
-    // 장바구니 수량 업데이트
-    @PostMapping("/updateQuantityCart")
-    public String updateQuantityCart(EditCart editCart) {
-        cartService.updateCart(editCart.toEntity());
-        return "redirect:/user/carts";
-    }
+	// 장바구니 수량 업데이트
+	@PostMapping("/updateQuantityCart")
+	public String updateQuantityCart(EditCart editCart) {
+		cartService.updateCart(editCart.toEntity());
+		return "redirect:/user/carts";
+	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "cart/cartViews";
+	}
 }
